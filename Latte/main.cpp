58ef81b8687fc92b6bb805cf21c61511d1630601
Paweł Czerwiński <22awik@gmail.com>
2019-12-21 14:35:07 +0100

    Initial Commit

---
+++ Latte/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Latte
+//
+//  Created by Paweł Czerwiński on 21/12/2019.
+//  Copyright © 2019 Paweł Czerwiński. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
