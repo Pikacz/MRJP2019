18ae33777bb78a1d4f32e56e3ebff42ce76ba50c
Paweł Czerwiński <22awik@gmail.com>
2020-01-21 15:23:10 +0100

    main

--- Latte/main.cpp
+++ Latte/main.cpp
@@ -5,10 +5,12 @@
 //  Created by Paweł Czerwiński on 21/12/2019.
 //  Copyright © 2019 Paweł Czerwiński. All rights reserved.
 //
-
-#include <iostream>
 #include "Logic.hpp"
 
+#include <iostream>
+#include <bitset>
+#include <string>
+#include <cstdio>
 #include <list>
 #include <memory>
 #include <vector>
@@ -17,53 +19,83 @@
 
 using namespace std;
 
+string getOutputBaseName(string str) {
+    string base;
+    size_t dot = str.find_last_of(".");
+    
+    if (dot == string::npos) {
+        base = str;
+    } else {
+        base = str.substr(0, dot);
+    }
+    
+    return base;
+}
+
+
+
 
 
 int main(int argc, const char * argv[]) {
+    
+    stringstream inputStream;
+    if (argc > 0) {
+        inputStream << argv[0];
+    }
+    string inName = inputStream.str();
+    string baseName = getOutputBaseName(inName);
+    
+    
+    cout << getOutputBaseName("") << endl;
+    
     stringstream ss;
     
-    ss << "class A { }" << endl;
-    ss << "class B extends A { }" << endl;
-    ss << "int main() {" << endl;
-    ss << "    int[] a;" << endl;
-    ss << "    printInt(a.length);" << endl;
-    ss << "    return a[1];" << endl;
-    ss << "}" << endl;
-    ss << "" << endl;
-
-    
+    ifstream input(inName);
+    ss << input.rdbuf();
     string str = ss.str();
     
-//    antlr4::ANTLRInputStream input(str);
-//    LatteLexer lexer(&input);
-//    antlr4::CommonTokenStream tokens(&lexer);
-//    tokens.fill();
-//    LatteParser parser(&tokens);
-//    cout.flush();
-//    auto defs = parser.main()->topDef();
-//    cout << defs.size() << endl;
-//
-//    auto m = defs[0]->function();
-//    auto name = m->Identifier()->getText();
-//    cout << name << endl;
+    
     
     try {
         auto env = TopDefFactory::createFrom(str);
         list<unique_ptr<const AsmInstruction>> compiled;
         env->compile(compiled);
         
+        cerr << "OK" << endl;
+        
+        string asmName = baseName + ".s";
+        ostream * asmStream = &cout;
+        if (baseName != "") {
+            asmStream = new ofstream(asmName);
+        }
         
         for (auto it = compiled.begin(); it != compiled.end(); it++) {
             stringstream ss;
             it->get()->write(ss);
-            cout << ss.str();
+            (*asmStream) << ss.str();
+        }
+        
+        if (baseName != "") {
+            ((ofstream *)asmStream)->close();
+            delete asmStream;
+            
+            string compile = "clang++ latte_lib/latte_lib.o" + asmName + " -o " + baseName;
+            
+            system(compile.c_str());
         }
         
     } catch (StaticCheckError & error) {
+        cerr << "ERROR" << endl;
         cerr << error.what();
     } catch (ParserError & error) {
+        cerr << "ERROR" << endl;
         cerr << error.what();
+    } catch (string & s) {
+        cerr << "OK" << endl;
+        cerr << "correct program but compilator is not finished" << endl;
+        cerr << s << endl;
     }
+    
     
     
     return 0;
